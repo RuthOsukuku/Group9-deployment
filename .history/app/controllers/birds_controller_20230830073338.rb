53b619a07c5b238efd5d05c66abed3387169b04c
Ruth <osukukuruth@gmail.com>
2023-08-30 07:38:30 -0400

    Done Deployment

--- .history/app/controllers/birds_controller_20230830073338.rb
+++ .history/app/controllers/birds_controller_20230830073338.rb
@@ -2,13 +2,13 @@ class BirdsController < ApplicationController
  
   def index
     birds = Bird.all
-    render json: birds, only: [:id, :name, :species]
+    render json: birds
   end
 
   
   def show
     bird = Bird.find(params[:id])
-    render json: bird, only: [:id, :name, :species]
+    render json: bird, except : [:created_at, :updated_at]
   end
 
   
